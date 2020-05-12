284813d47c90c55b8fa2cbb891590e719ca1fc76
Andrês <andresroliveira98@gmail.com>
2020-05-12 00:40:48 -0300

    Update generator.cpp

--- Instances/generator.cpp
+++ Instances/generator.cpp
@@ -18,11 +18,11 @@
 #define db2(x, y) cerr << #x << " = " << x << " || " << #y << " = " << y << '\n';
 #define db3(x, y, z) cerr << #x << " = " << x << " || " << #y << " = " << y << " || " << #z << " = " << z << '\n';
 #define db4(x, y, z, w) cerr << #x << " = " << x << " || " << #y << " = " << y << " || " << #z << " = " << z << " || " << #w << " = " << w << '\n';
+#define dbp(p) cerr << #p << " = ("<< p.first << ", " << p.second << ")\n";
 #define toRad(a) (a)*PI/180.0
 #define myabs(a) ((a) < 0 ? -(a) : (a))
 #define mdc __gcd
 #define edge pair<int, pair<int, double> >
-
 
 using namespace std;
 
@@ -137,9 +137,9 @@ double realRand(double a, double b){
     return a + (b - a)*r;
 }
 
-double d[NMAX+1]; 
-double t[NMAX+1]; 
-double w[NMAX+1]; 
+double d[NMAX+1];
+double t[NMAX+1];
+double w[NMAX+1];
 
 void fisherYatesShuffle(int vet[], int n){
     for(int i=0;i<n;i++) vet[i] = i;
@@ -155,12 +155,121 @@ void fisherYatesShuffle(int vet[], int n){
 
 int pos[30*NMAX];
 
+vector<edge> getPruneEdges(int n, int ns, int mp, vector<edge> vet){
+    vi S;
+    S.push_back(0);
+    S.push_back(4);
+
+    int n_aux = mp - ns;
+
+    n_aux = (n_aux > 0) ? 0 : myabs(n_aux);
+
+    vi shuf; for(int i=5+abs(n_aux);i<=n;i++) shuf.push_back(i);
+    random_shuffle(all(shuf));
+
+    for(int i=5;i<=4+abs(n_aux);i++) S.push_back(i);
+    for(int j=0;j<ns-abs(n_aux)-1;j++){
+        S.push_back(shuf[j]);
+    }
+
+    sort(all(S));
+
+    int ns_aux = ns; vi S_aux = S;
+
+    if(S[ns] != n){
+        S_aux.push_back(n);
+        ns_aux++;
+    }
+
+    vii Eps;
+
+    for(int i=2;i<ns_aux;i++){
+        if(S_aux[i] != S_aux[i-1] + 1){
+            Eps.push_back(make_pair(S_aux[i-1]+1-4, S_aux[i]-1));
+        }
+    }
+
+    if(n != S[ns-1]+1){
+        if(S[ns] == n){
+            Eps.push_back(make_pair(S_aux[ns_aux-1]+1-4, S_aux[ns_aux]-1));
+        }
+        else{
+            Eps.push_back(make_pair(S_aux[ns_aux-1]+1-4, S_aux[ns_aux]));
+        }
+    }
+
+    if(sz(Eps) >= 2 && Eps[0] == Eps[1]) Eps.pop_back();
+
+    int m_Eps = (int)sz(Eps);
+
+    vii boxes;
+    queue <int> fila;
+
+    for(int k=0;k<sz(Eps);k++){
+        int ei = Eps[k].second - Eps[k].first;
+        int cap = (ei*ei-5*ei + 6)/2;
+        boxes.push_back(make_pair(0, cap - 1));
+        fila.push(k);
+    }
+
+    int cont = mp - m_Eps;
+
+    while(!fila.empty() && cont > 0){
+        int u = fila.front(); fila.pop();
+
+        if(boxes[u].first >= boxes[u].second) continue;
+
+        fila.push(u);
+        boxes[u].first++;
+        cont--;
+    }
+
+    double r = 25.0;
+    int andres = 0, wagner = 0;
+
+    for(int k=0;k<m_Eps;k++){
+        int a = Eps[k].first;
+        int b = Eps[k].second;
+        double d = distance(X[a], X[b]);
+        edge e = make_pair(a,make_pair(b,sqrt(d)));
+        vet.push_back(e);
+
+        cont = boxes[k].first;
+
+        for(int i=a+4;i<=b && cont > 0;i++){
+            for(int j=a;j<=i-4 && cont > 0;j++){
+                d = distance(X[i], X[j]);
+                if(j == a && i == b) continue;
+
+                if(d < r) {
+                    e = make_pair(j,make_pair(i,sqrt(d)));
+                    vet.push_back(e);
+                    cont--;
+                }
+            }
+        }
+
+        if(cont > 0){
+            for(int i=a+4;i<=b && cont > 0;i++){
+                for(int j=a;j<=i-4 && cont > 0;j++){
+                    if(j == a && i == b) continue;
+                    d = distance(X[i], X[j]);
+                    if(d >= r) {
+                        e = make_pair(j,make_pair(i,sqrt(d)));
+                        vet.push_back(e);
+                        cont--;
+                    }
+                }
+            }
+        }
+    }
+    return vet;
+}
+
 int main(){
 
     cerr << "Entre com um caso: "; int caso; cin >> caso; for(int i=1;i<=caso;i++) rand();
-    cerr << "Entre com o numero de atomos: ";
-    int n; cin >> n;
-
+    cerr << "Entre com o numero de atomos: "; int n; cin >> n;
     for(int i=1;i<=n;i++) d[i] = 1.526; d[1] = 0;
     for(int i=1;i<=n;i++) t[i] = 1.91; t[1] = t[2] = 0;
     for(int i=4;i<=n;i++) {
@@ -168,8 +277,8 @@ int main(){
         w[i] = omega;
     }
     w[1] = w[2] = w[3] = 0;
-    
-    PosicaoPontos(d, t, w, n); 
+
+    PosicaoPontos(d, t, w, n);
     vector<edge> vet;
 
     for(int i=1;i<=n;i++){
@@ -180,39 +289,10 @@ int main(){
         }
     }
 
-    cerr << "Entre com a quantidade de simetrias: ";
-    int sym; cin >> sym;
+    cerr << "Entre com a quantidade de simetrias: "; int ns;  cin >> ns;
+    cerr << "Entre com a quantidade de podas: "; int mp; cin >> mp;
 
-    vi shuf; for(int i=5;i<=n;i++) shuf.push_back(i);
-    random_shuffle(all(shuf));
-
-    vi S; S.push_back(4);
-    for(int j=0;j<sym-1;j++){
-        S.push_back(shuf[j]);
-    }
-
-    S.push_back(n+1);
-
-    sort(all(S));
-    for(int j=0;j<sz(S)-1;j++) cerr << S[j] << ' '; cerr << "\n\n";
-
-    double r = 25.0; 
-
-    for(int k=1;k<sz(S);k++){
-        int a = S[k-1];
-        int b = S[k];
-
-        for(int j=a+1;j<b;j++){
-            for(int i=a-3;i+3<j;i++){
-                double d = distance(X[i], X[j]);
-
-                if(d < r) {
-                    edge e = make_pair(i,make_pair(j,sqrt(d)));
-                    vet.push_back(e);
-                }
-            }
-        }
-    }
+    vet = getPruneEdges(n, ns, mp, vet);
 
     int m = sz(vet);
 
@@ -222,7 +302,7 @@ int main(){
 
     for(int i=0;i<m;i++){
         edge atu = vet[pos[i]];
-        
+
         int u = atu.first;
         int v = atu.second.first;
         double d = atu.second.second;
